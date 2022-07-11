b68f7ad705ecda92415c9146c424d501c132b2f0
meta-ela <anxhelameta@live.it>
2022-07-11 15:55:33 +0200

    Adds basic layout

--- index.php
+++ index.php
@@ -136,14 +136,40 @@ $db = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
+    <link rel="stylesheet" href="style.css">
     <title>Google Faq</title>
 </head>
 
 <body>
+    <header class="container-fluid fixed-top">
+        <nav class="py-3">
+            <div>
+                <div class="d-flex gap-4">
+                    <img src="google_logo.svg.png" alt="Google Logo">
+                    <h4>Privacy & Termini</h4>
+                </div>
+                <div>
+                    <!-- avatar account -->
+                </div>
+            </div>
+            <div>
+                <!-- link navbar -->
+            </div>
+        </nav>
+    </header>
 
+    <main class="vh-100">
+        
+    </main>
+
+    <footer class="py-3 fixed-bottom"></footer>
 </body>
 
 </html>
